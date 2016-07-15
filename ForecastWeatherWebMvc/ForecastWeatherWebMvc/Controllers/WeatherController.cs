31e5596cd7e5ff8d048f47c1b86cfc63794ecb02
Dudka <andriy.dudka@gmail.com>
2016-07-16 01:42:23 +0300

    add new view

--- ForecastWeatherWebMvc/ForecastWeatherWebMvc/Controllers/WeatherController.cs
+++ ForecastWeatherWebMvc/ForecastWeatherWebMvc/Controllers/WeatherController.cs
@@ -22,12 +22,13 @@ namespace ForecastWeatherApp.Controllers
 
         // POST: /Weather/Index
         [HttpPost]
-        public ActionResult Index(int days, string city, string searchcity)
-        {
-            Forecast forecast = searchcity == ""
+        public ActionResult ForecastWeather(int days, string city, string searchcity, string submit, string edit)
+        {          
+            Forecast forecast = string.IsNullOrEmpty(searchcity)
                 ? _weatherForecastService.GetForecast(city, days)
                 : _weatherForecastService.GetForecast(searchcity, days);
-            return View("ForecastWeather", forecast);
+
+            return string.IsNullOrEmpty(submit) ? View("EditListOfCity") : View(forecast);
         }
     }
 }
